8bc4fde34232b939e79c5cc74c1e17e0dbe92a4c
tomofumi <tomofumi@tomofuminoMacBook-Air.local>
2024-05-29 06:40:15 +0900

    タイトル画像表示

--- src/resources/views/baby/commons/menu.blade.php
+++ src/resources/views/baby/commons/menu.blade.php
@@ -1,1 +1,34 @@
-<p><a href="{{ route('baby.type', ['type' => 'sticker',]) }}">ステッカータイプ</a>　マグネットタイプ　吸盤タイプ</p>
+
+<div class="container container-m">
+<div class="card-deck">
+  <div class="card">
+   <div class="card-header">
+   <b><a href="{{ route('baby.type', ['type' => 'sticker',]) }}">ステッカー</a></b>
+   </div>
+   <div class="card-body">
+        <p class="card-text"><i class="fas fa-check-circle"></i> どこでも貼れる、種類が多い</p>
+        <p class="card-text"><i class="fas fa-times-circle"></i> 貼るのが難しい、貼り直しができない</p>
+   </div>
+  </div>
+  <div class="card">
+   <div class="card-header">
+   <b><a href="{{ route('baby.type', ['type' => 'magnet',]) }}">マグネット</b>
+   </div>
+    <div class="card-body">
+        <p class="card-text"><i class="fas fa-check-circle"></i> 気軽に貼り替え可能</p>
+        <p class="card-text"><i class="fas fa-times-circle"></i> 鉄板部分にしか貼れない</p>
+    </div>
+  </div>
+  <div class="card">
+   <div class="card-header">
+   <b><a href="{{ route('baby.type', ['type' => 'sucker',]) }}">吸盤</b>
+   </div>
+    <div class="card-body">
+        <p class="card-text"><i class="fas fa-check-circle"></i> 室内から貼るので盗まれない</p>
+        <p class="card-text"><i class="fas fa-times-circle"></i> スモークガラスの場合、外から見えづらい</p>
+    </div>
+  </div>
+</div>
+</div>
+
+<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
--- src/resources/views/baby/index.blade.php
+++ src/resources/views/baby/index.blade.php
@@ -2,13 +2,20 @@
 
 @section('content')
 
-{{--検索BOX--}}
-<form class="input-group col-md-5" action="{{ route('baby.result')}}" method="GET"> 
-  <input type="search" name="keyword" class="form-control input-group-prepend" placeholder="キーワード"></input>
-  <span class="input-group-btn input-group-append">
-    <input type="submit" class="btn btn-primary"  value="検索">
-  </span>
-</form>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="../../babyincar.jpeg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">車用BabyInCarを紹介するサイト</h5>
+        <p class="card-text">子供が生まれたらマイカーに必ず貼りたい"子供が乗ってます"ステッカーのポータルサイトです。</p>
+        <p class="card-text"><small class="text-body-secondary">そもそもBabyInCarステッカーが生まれた理由とは?</small></p>
+      </div>
+    </div>
+  </div>
+</div>
 
 @include('baby.commons.menu')
 
@@ -19,9 +26,9 @@
 
       <div class="col-xl-4">
           <a href="{{ $item['itemUrl'] }}" class="thumbnail">
-            <img src="{{ $item['mediumImageUrls'][0]['imageUrl'] }}" align="left" hspace="20" vspace="20">
+            <img src="{{ $item['mediumImageUrls'][0]['imageUrl'] }}" align="left" hspace="20" vspace="20"></br>
             {{ $item['itemPrice'] }}円</br>
-            {{ $item['reviewCount'] }}件
+            <i class="far fa-comments fa-lg"></i>{{ $item['reviewCount'] }}件
           </a>
       </div>
 
@@ -29,5 +36,15 @@
 
   </div>
 </div>
+
+
+
+{{--検索BOX--}}
+<form class="input-group col-md-5" action="{{ route('baby.result')}}" method="GET"> 
+  <input type="search" name="keyword" class="form-control input-group-prepend" placeholder="キーワード"></input>
+  <span class="input-group-btn input-group-append">
+    <input type="submit" class="btn btn-primary"  value="検索">
+  </span>
+</form>
 
 @endsection
