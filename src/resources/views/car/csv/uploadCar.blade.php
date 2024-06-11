b16c0c54467528c4ad28dfe8c9f46ff3e5193cee
tomofumi <tomofumi@tomofuminoMacBook-Air.local>
2024-06-12 07:36:24 +0900

    https以外完成

--- src/resources/views/car/csv/uploadCar.blade.php
+++ src/resources/views/car/csv/uploadCar.blade.php
@@ -8,6 +8,7 @@
     <div class="row">
         <div class="col-6">
             {!! Form::open(['route' => 'csv.importCar','files'=>true]) !!}
+
             {{--<form method="POST" action="https://localhost/car/csv/import" enctype="multipart/form-data">--}}
             @csrf
                 <div class="form-group">
@@ -16,7 +17,7 @@
                 </div>
 
                 @if (session('flash_message'))
-                    <div class="alert alert-success">
+                    <div class="alert alert-success">git 
                         {{ session('flash_message') }}
                     </div>
                 @endif
