5e1f102300cada9056ebaf78266ec5fdbf4ec422
tomofumi <tomofumi@tomofuminoMacBook-Air.local>
2025-02-12 07:25:28 +0900

    不要ファイル削除

--- src/resources/views/quote/form.blade.php
+++
@@ -1,6 +0,0 @@
-{!! Form::open(['route' => 'quotes.store']) !!}
-    <div class="form-group">
-        {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '2']) !!}
-        {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
-    </div>
-{!! Form::close() !!}
