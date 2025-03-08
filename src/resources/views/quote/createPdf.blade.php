8e32d059a6085aeeae42d1796a55dcf9044d62f7
tomofumi <tomofumi@tomofuminoMacBook-Air.local>
2025-03-09 06:47:07 +0900

    見積もりpdfの外枠削除

--- src/resources/views/quote/createPdf.blade.php
+++ src/resources/views/quote/createPdf.blade.php
@@ -6,21 +6,12 @@
         body {
             font-family: Arial, sans-serif;
             font-size: 12px;
-            margin: 0;
+            margin: 20px;
             padding: 0;
             background-color: #f8f8f8;
         }
         @page {
             margin: 20px;
-        }
-        .container {
-            width: 100%;
-            max-width: 900px;
-            margin: 20px auto;
-            background: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
         .header {
             background-color: #d82c2c;
@@ -71,116 +62,113 @@
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="header">御見積書</div>
-        <div class="section flex">
-
-            <div style="text-align: right;">
-                <strong>発行日:</strong> {{ $date }}
-            </div>
+    <div class="header">御見積書</div>
+    <div class="section flex">
+        <div style="text-align: right;">
+            <strong>発行日:</strong> {{ $date }}
         </div>
+    </div>
 
-        <div class="section">
-            <table class="table details-table">
-                <tbody>
-                    <tr>
-                        <th>車名</th><th>年式</th><th>走行距離</th><th>車検</th>
-                    </tr>
-                    <tr>
-                        <td>{{ $car ?? '' }} {{ $displacement ?? '' }} {{ $transmission ?? '' }} {{ $color ?? '' }} {{ $drive ?? '' }}</td>
-                        <td>{{ $year ?? '' }}</td>
-                        <td>{{ $mileage ?? '' }} km</td>
-                        <td>{{ $inspection ?? '' }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="section">
+        <table class="table details-table">
+            <tbody>
+                <tr>
+                    <th>車名</th><th>年式</th><th>走行距離</th><th>車検</th>
+                </tr>
+                <tr>
+                    <td>{{ $car ?? '' }} {{ $displacement ?? '' }} {{ $transmission ?? '' }} {{ $color ?? '' }} {{ $drive ?? '' }}</td>
+                    <td>{{ $year ?? '' }}</td>
+                    <td>{{ $mileage ?? '' }} km</td>
+                    <td>{{ $inspection ?? '' }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
-        <div class="section">
-            <table class="table details-table">
-                <tr>
-                    <th colspan="2">項目</th>
-                    <th>金額</th>
-                </tr>
-                <tr>
-                    <td colspan="2">車輌本体価格①（消費税込み）</td>
-                    <td style="text-align: right;">{{ number_format($price ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td class="narrow-column" rowspan="5">諸費用②</td>
-                    <td>自動車税（月割）</td>
-                    <td style="text-align: right;">{{ number_format($tax_1 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>重量税</td>
-                    <td style="text-align: right;">{{ number_format($tax_2 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>自賠責保険料</td>
-                    <td style="text-align: right;">{{ number_format($tax_3 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>リサイクル預託金</td>
-                    <td style="text-align: right;">{{ number_format($tax_5 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>登録費用</td>
-                    <td style="text-align: right;">{{ number_format($overhead_1 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td class="narrow-column" rowspan="5">オプションその他③</td>
-                    <td>{{ $optionName_1 ?? '' }}</td>
-                    <td style="text-align: right;">{{ number_format($option_1 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>{{ $optionName_2 ?? '' }}</td>
-                    <td style="text-align: right;">{{ number_format($option_2 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>{{ $optionName_3 ?? '' }}</td>
-                    <td style="text-align: right;">{{ number_format($option_3 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>{{ $optionName_4 ?? '' }}</td>
-                    <td style="text-align: right;">{{ number_format($option_4 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td>{{ $optionName_5 ?? '' }}</td>
-                    <td style="text-align: right;">{{ number_format($option_5 ?? 0) }} 円</td>
-                </tr>
-                <tr>
-                    <td colspan="2">総 合 計（①＋②＋③）</td>
-                    <td class="highlight" style="text-align: right;">{{ number_format($total ?? 0) }} 円</td>
-                </tr>
-            </table>
-        </div>
+    <div class="section">
+        <table class="table details-table">
+            <tr>
+                <th colspan="2">項目</th>
+                <th>金額</th>
+            </tr>
+            <tr>
+                <td colspan="2">車輌本体価格①（消費税込み）</td>
+                <td style="text-align: right;">{{ number_format($price ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td class="narrow-column" rowspan="5">諸費用②</td>
+                <td>自動車税（月割）</td>
+                <td style="text-align: right;">{{ number_format($tax_1 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>重量税</td>
+                <td style="text-align: right;">{{ number_format($tax_2 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>自賠責保険料</td>
+                <td style="text-align: right;">{{ number_format($tax_3 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>リサイクル預託金</td>
+                <td style="text-align: right;">{{ number_format($tax_5 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>登録費用</td>
+                <td style="text-align: right;">{{ number_format($overhead_1 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td class="narrow-column" rowspan="5">オプションその他③</td>
+                <td>{{ $optionName_1 ?? '' }}</td>
+                <td style="text-align: right;">{{ number_format($option_1 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>{{ $optionName_2 ?? '' }}</td>
+                <td style="text-align: right;">{{ number_format($option_2 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>{{ $optionName_3 ?? '' }}</td>
+                <td style="text-align: right;">{{ number_format($option_3 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>{{ $optionName_4 ?? '' }}</td>
+                <td style="text-align: right;">{{ number_format($option_4 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td>{{ $optionName_5 ?? '' }}</td>
+                <td style="text-align: right;">{{ number_format($option_5 ?? 0) }} 円</td>
+            </tr>
+            <tr>
+                <td colspan="2">総 合 計（①＋②＋③）</td>
+                <td class="highlight" style="text-align: right;">{{ number_format($total ?? 0) }} 円</td>
+            </tr>
+        </table>
+    </div>
 
-        <div class="section">
-            <table class="table details-table">
-                <tbody>
-                    <tr>
-                        <td>下取り</td>
-                        <td style="text-align: right;">{{ number_format($trade_price ?? 0) }} 円</td>
-                    </tr>
-                    <tr>
-                        <td>値引き</td>
-                        <td style="text-align: right;">{{ number_format($discount ?? 0) }} 円</td>
-                    </tr>
-                    <tr>
-                        <td>お支払い合計</td>
-                        <td class="highlight" style="text-align: right;">{{ number_format($payment ?? 0) }} 円</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="section">
+        <table class="table details-table">
+            <tbody>
+                <tr>
+                    <td>下取り</td>
+                    <td style="text-align: right;">{{ number_format($trade_price ?? 0) }} 円</td>
+                </tr>
+                <tr>
+                    <td>値引き</td>
+                    <td style="text-align: right;">{{ number_format($discount ?? 0) }} 円</td>
+                </tr>
+                <tr>
+                    <td>お支払い合計</td>
+                    <td class="highlight" style="text-align: right;">{{ number_format($payment ?? 0) }} 円</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
-        <div class="section">
-            <strong>備考:</strong>
-        </div>
+    <div class="section">
+        <strong>備考:</strong>
+    </div>
 
-        <div class="footer">
-            <p>※見積もり有効期限は発行から１週間</p>
-        </div>
+    <div class="footer">
+        <p>※見積もり有効期限は発行から１週間</p>
     </div>
 </body>
 </html>
