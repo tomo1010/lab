08d805303335934b0ddf557e9058d36cdc8ccb74
tomofumi <tomofumi@tomofuminoMacBook-Air.local>
2025-03-11 06:29:28 +0900

    マイグレファイルに諸費用の名称追加

--- src/database/migrations/2025_02_04_072931_create_quotes_table.php
+++ src/database/migrations/2025_02_04_072931_create_quotes_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('quotes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+
             //ユーザ情報
             $table->string('name')->nullable(); //お客様名
             $table->string('post')->nullable(); //郵便番号
@@ -35,54 +36,11 @@ return new class extends Migration
             $table->string('mileage')->nullable(); //走行距離
             $table->string('inspection')->nullable(); //車検日
 
+
             //車輌価格
             $table->integer('price');
             $table->integer('discount')->default(0);
 
-            //オプション
-            $table->integer('option_1')->default(0);
-            $table->integer('option_2')->default(0);
-            $table->integer('option_3')->default(0);
-            $table->integer('option_4')->default(0);
-            $table->integer('option_5')->default(0);
-            $table->integer('option_6')->default(0);
-            $table->integer('option_7')->default(0);
-            $table->integer('option_8')->default(0);
-            $table->integer('option_9')->default(0);
-            $table->integer('option_10')->default(0);
-            $table->integer('option_11')->default(0);
-            $table->integer('option_12')->default(0);
-            $table->integer('option_13')->default(0);
-            $table->integer('option_14')->default(0);
-            $table->integer('option_15')->default(0);
-            $table->integer('option_16')->default(0);
-            $table->integer('option_17')->default(0);
-            $table->integer('option_18')->default(0);
-            $table->integer('option_19')->default(0);
-            $table->integer('option_20')->default(0);
-            $table->integer('option_total')->default(0);
-
-            //オプション名称
-            $table->string('optionName_1')->nullable();
-            $table->string('optionName_2')->nullable();
-            $table->string('optionName_3')->nullable();
-            $table->string('optionName_4')->nullable();
-            $table->string('optionName_5')->nullable();
-            $table->string('optionName_6')->nullable();
-            $table->string('optionName_7')->nullable();
-            $table->string('optionName_8')->nullable();
-            $table->string('optionName_9')->nullable();
-            $table->string('optionName_10')->nullable();
-            $table->string('optionName_11')->nullable();
-            $table->string('optionName_12')->nullable();
-            $table->string('optionName_13')->nullable();
-            $table->string('optionName_14')->nullable();
-            $table->string('optionName_15')->nullable();
-            $table->string('optionName_16')->nullable();
-            $table->string('optionName_17')->nullable();
-            $table->string('optionName_18')->nullable();
-            $table->string('optionName_19')->nullable();
-            $table->string('optionName_20')->nullable();
 
             //税金・保険料など
             $table->integer('tax_1')->default(0); //自動車税
@@ -113,6 +71,12 @@ return new class extends Migration
             $table->integer('overhead_14')->default(0);
             $table->integer('overhead_15')->default(0);
             $table->integer('overhead_total')->default(0);
+            //諸費用名称
+            $table->integer('overheadName_11')->default(0); 
+            $table->integer('overheadName_12')->default(0);
+            $table->integer('overheadName_13')->default(0);
+            $table->integer('overheadName_14')->default(0);
+            $table->integer('overheadName_15')->default(0);
             //非課税　諸費用
             $table->integer('exempt_1')->default(0); //車庫証明証紙
             $table->integer('exempt_2')->default(0); //検査登録届出
@@ -120,9 +84,57 @@ return new class extends Migration
             $table->integer('exempt_4')->default(0);
             $table->integer('exempt_5')->default(0);
             $table->integer('exempt_total')->default(0);
+
+
+            //オプション
+            $table->integer('option_1')->default(0);
+            $table->integer('option_2')->default(0);
+            $table->integer('option_3')->default(0);
+            $table->integer('option_4')->default(0);
+            $table->integer('option_5')->default(0);
+            $table->integer('option_6')->default(0);
+            $table->integer('option_7')->default(0);
+            $table->integer('option_8')->default(0);
+            $table->integer('option_9')->default(0);
+            $table->integer('option_10')->default(0);
+            $table->integer('option_11')->default(0);
+            $table->integer('option_12')->default(0);
+            $table->integer('option_13')->default(0);
+            $table->integer('option_14')->default(0);
+            $table->integer('option_15')->default(0);
+            $table->integer('option_16')->default(0);
+            $table->integer('option_17')->default(0);
+            $table->integer('option_18')->default(0);
+            $table->integer('option_19')->default(0);
+            $table->integer('option_20')->default(0);
+            $table->integer('option_total')->default(0);
+            //オプション名称
+            $table->string('optionName_1')->nullable();
+            $table->string('optionName_2')->nullable();
+            $table->string('optionName_3')->nullable();
+            $table->string('optionName_4')->nullable();
+            $table->string('optionName_5')->nullable();
+            $table->string('optionName_6')->nullable();
+            $table->string('optionName_7')->nullable();
+            $table->string('optionName_8')->nullable();
+            $table->string('optionName_9')->nullable();
+            $table->string('optionName_10')->nullable();
+            $table->string('optionName_11')->nullable();
+            $table->string('optionName_12')->nullable();
+            $table->string('optionName_13')->nullable();
+            $table->string('optionName_14')->nullable();
+            $table->string('optionName_15')->nullable();
+            $table->string('optionName_16')->nullable();
+            $table->string('optionName_17')->nullable();
+            $table->string('optionName_18')->nullable();
+            $table->string('optionName_19')->nullable();
+            $table->string('optionName_20')->nullable();
+
+
             //合計
             $table->integer('subtotal')->default(0);
             $table->integer('total')->default(0);
+
 
             //下取り
             $table->string('trade_maker')->nullable(); //メーカー
@@ -138,7 +150,6 @@ return new class extends Migration
             $table->string('trade_inspection')->nullable(); //車検
             $table->string('trade_mileage')->nullable(); //走行距離
             $table->integer('trade_price')->default(0);
-            
             //支払い条件
             $table->integer('payments')->default(0); //支払い回数
             $table->integer('first')->default(0); //初回
@@ -148,8 +159,10 @@ return new class extends Migration
             $table->integer('residual')->default(0); //残価設定
             $table->integer('cash')->default(0); //頭金
 
+
             $table->integer('payment')->default(0); //最終支払い総額
             
+
             //メモ
             $table->string('memmo')->nullable(); //メモ
 
