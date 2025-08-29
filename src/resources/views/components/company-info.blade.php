339f140dbb3b5098881c3aff557404e92110b66a
Seiko Tsubono <>
2025-08-29 19:08:01 +0900

    原価入力・粗利設定をCookieに自動保存

--- src/resources/views/components/company-info.blade.php
+++ src/resources/views/components/company-info.blade.php
@@ -166,22 +166,6 @@
                     alert('クッキーから発信者情報を削除しました。');
                 }
             });
-
-            function setCookie(name, value, days) {
-                const expires = new Date(Date.now() + days * 864e5).toUTCString();
-                document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
-            }
-
-            function getCookie(name) {
-                return document.cookie.split('; ').reduce((r, v) => {
-                    const parts = v.split('=');
-                    return parts[0] === name ? decodeURIComponent(parts[1]) : r;
-                }, '');
-            }
-
-            function deleteCookie(name) {
-                setCookie(name, '', -1);
-            }
         </script>
         @endguest
 
