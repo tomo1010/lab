194616ee87e43551dcfa64437e14cc37b664a38f
tomofumi <tomofumi@tomofuminoMacBook-Air.local>
2025-10-26 21:14:24 +0900

    編集ページタイトルに（編集）追加

--- src/resources/views/quote/edit.blade.php
+++ src/resources/views/quote/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            クイック見積もり
+            クイック見積もり（編集）
         </h2>
     </x-slot>
 
